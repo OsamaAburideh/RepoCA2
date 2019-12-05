04e832355457af49b4ef0e41e44baf34a87cf986
Osama <x00154068@mytudublin.ie>
2019-12-05 03:08:08 +0000

    Added Orders and some navbar features

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'crispy_forms',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
--- shop_project/urls.py
+++ shop_project/urls.py
@@ -22,6 +22,7 @@ from shop import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
     path('account/create/', views.signupView, name='signup'),
     path('account/login/', views.signinView, name='signin'),
